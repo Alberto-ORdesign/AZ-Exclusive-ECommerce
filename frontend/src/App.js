4dc80d79a8964c5a05c1bc3a5802f02fde8aa0d3
Alberto <alberto@MacBook-Pro-de-olivier.local>
2022-03-08 12:55:23 +0100

    primer producto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import data from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">AZ Exclusive</a>
       </header>
-      <main>list products</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.imagen} alt={product.nombre} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.nombre}</p>
+                </a>
+                <p>
+                  <strong>{product.precio}€</strong>
+                </p>
+                <button>Añadir al carrito</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
